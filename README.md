ec34928d439102f06e4423f2d00db120e1504558
Jorge Chocobar <jorge1986e@gmail.com>
2026-09-14 16:41:37 -0300

    post para gregar instrumentos

---
+++ README.md
@@ -0,0 +1,14 @@
+# Trabajo práctico 03
+## Descripción
+## Instalación
+## Ejecución
+## Endpoints
+## Ejemplos de solicitudes
+## Códigos de estado
+## Persistencia de los datos
+
+
+Debe explicar:
+npm install y npm start ;- cómo detener el servidor;- método y URL de cada endpoint;- cuerpo necesario para POST;
+casos 200 , 201 , 400 y 404 ;- diferencia entre parámetro de ruta y consulta;
+función de express.json() ;- por qué las creaciones desaparecen al reiniciar
--- src/index.js
+++ src/index.js
@@ -28,16 +28,39 @@ async function main()
         res.json(resultado);
     });
 
-//    -----------------------     //
+//    -----------------------busca x id     //
 
 app.get("/api/instrumentos/:id", (req, res)=>{
     const id = Number(req.params.id);
     const instrumento = instrumentos.find((elemento) => elemento.id === id);
     if (!instrumento) 
-    {
-     return res.status(404).json({error: "instrumento no encontrado"})   
-    }
-    res.status(200).json(instrumento);
+        {
+            return res.status(404).json({error: "instrumento no encontrado"})   
+        }
+        res.status(200).json(instrumento);
+    });
+    
+    //    -----------------------POST     //
+
+app.post("/api/instrumentos", (req, res) =>{
+const { nombre, familia, origen, descripcion, disponible } = req.body;
+
+if (!nombre || !familia || !origen || !descripcion || disponible === undefined) {
+    return res.status(400).json({error: "faltan datos obligatorios"});
+}
+
+const ultimoId = instrumentos.length === 0 ? 0 : instrumentos[instrumentos.length -1].id;
+const nuevoInstrumento = {
+id: ultimoId + 1,
+nombre,
+familia,
+origen,
+descripcion,
+disponible,};
+
+instrumentos.push(nuevoInstrumento);
+res.status(201).json(nuevoInstrumento);
+
 });
 
     app.listen(PORT, ()=>{
